5010f5a8d5d07ef4f0df4a577f0308428939cb28
Dinesh R <dineshruthwik21@gmail.com>
2024-01-31 10:28:07 +0530

    Create Odd Or Even.c

---
+++ Odd Or Even.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    int num;
+    
+    num = 11;
+    
+    if (num % 2 == 0) {
+        printf("Even");
+    } else {
+        printf("Odd");
+    }
+
+    return 0;
+}
+             
